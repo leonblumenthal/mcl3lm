ab8cf2f73736a02340c5140682e871c9ef797e5c
Leon Blumenthal <dev.blumenthal.leon@gmail.com>
2023-01-29 22:04:02 +0100

    WIP eval

--- evaluation.cpp
+++ evaluation.cpp
@@ -36,48 +36,142 @@
 #include "visual_odometry.h"
 #include "io_utils.h"
 #include "alignment.h"
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
 
 namespace fs = std::filesystem;
 
-int main() {
+int main(int argc, char *argv[]) {
 
   ////////////////////////////////////////////////////////////////
   // Config
 
+  const fs::path output_path{argv[1]};
+  std::ifstream config_data_file(argv[2]);
+  const json config_data = json::parse(config_data_file);
+  config_data_file.close();
+
   // General
-  const fs::path output_path{"../out/test"};
-  const bool verbose{true};
-  const bool visual{false};
+  const bool verbose{config_data["verbose"]};
+  const bool visual{config_data["visual"]};
+  const bool do_dump_map{config_data["do_dump_map"]};
+
 
   // Geometric map
-  const std::string ply_path{"../data/euroc_mav/V1_01_easy/pointcloud0/data.ply"};
-  const Bounds vertices_bounds{-4.73, -3.49, -0.04, 4.10, 5.16, 4.13};
-  const double voxel_size{0.25};
+  const std::string ply_path{config_data["geometric_map"]["ply_path"]};
+  const Bounds vertices_bounds{
+      config_data["geometric_map"]["vertices_bounds"]["min_x"],
+      config_data["geometric_map"]["vertices_bounds"]["min_y"],
+      config_data["geometric_map"]["vertices_bounds"]["min_z"],
+      config_data["geometric_map"]["vertices_bounds"]["max_x"],
+      config_data["geometric_map"]["vertices_bounds"]["max_y"], config_data["geometric_map"]["vertices_bounds"]["max_z"]
+  };
+  const double voxel_size{config_data["geometric_map"]["voxel_size"]};
+
 
   // Visual odometry;
-  const std::string images_directory{"../data/euroc_mav/V1_01_easy/cam0/data"};
-  const std::string images_ending{"png"};
+  const std::string images_directory{config_data["visual_odometry"]["images_directory"]};
+  const std::string images_ending{config_data["visual_odometry"]["images_ending"]};
   const Camera<double> camera{
-      752.0, 480.0, {458.654, 457.296, 367.215, 248.375, -0.28340811, 0.07395907, 0.00019359, 1.76187114e-05}};
-  const int initial_keyframe_index_1{100}, initial_keyframe_index_2{120};
-  const Pose initial_pose_1
-      {Eigen::Quaterniond{-0.4277372, 0.62498984, -0.54384451, 0.36147164}, {0.87029955, 2.2052097, 0.92827237}};
-  const Pose initial_pose_2
-      {Eigen::Quaterniond{-0.44759128, 0.60619323, -0.53792524, 0.377926253}, {0.97336398, 2.25578283, 1.06085795}};
-  const VisualOdometryConfig visual_odometry_config{};
+      config_data["visual_odometry"]["camera"]["width"], config_data["visual_odometry"]["camera"]["height"], {
+          config_data["visual_odometry"]["camera"]["parameters"]["fx"],
+          config_data["visual_odometry"]["camera"]["parameters"]["fy"],
+          config_data["visual_odometry"]["camera"]["parameters"]["cx"],
+          config_data["visual_odometry"]["camera"]["parameters"]["cy"],
+          config_data["visual_odometry"]["camera"]["parameters"]["k1"],
+          config_data["visual_odometry"]["camera"]["parameters"]["k2"],
+          config_data["visual_odometry"]["camera"]["parameters"]["p1"],
+          config_data["visual_odometry"]["camera"]["parameters"]["p2"]
+      }};
+  const int initial_keyframe_index_1{config_data["visual_odometry"]["initial_keyframe_index_1"]},
+      initial_keyframe_index_2{config_data["visual_odometry"]["initial_keyframe_index_2"]};
+  const Pose initial_pose_1{
+      Eigen::Quaterniond{
+          config_data["visual_odometry"]["initial_pose_1"]["qw"],
+          config_data["visual_odometry"]["initial_pose_1"]["qx"],
+          config_data["visual_odometry"]["initial_pose_1"]["qy"], config_data["visual_odometry"]["initial_pose_1"]["qz"]
+      }, Eigen::Vector3d(
+          config_data["visual_odometry"]["initial_pose_1"]["x"],
+          config_data["visual_odometry"]["initial_pose_1"]["y"],
+          config_data["visual_odometry"]["initial_pose_1"]["z"]
+      )
+  };
+  const Pose initial_pose_2{
+      Eigen::Quaterniond{
+          config_data["visual_odometry"]["initial_pose_2"]["qw"],
+          config_data["visual_odometry"]["initial_pose_2"]["qx"],
+          config_data["visual_odometry"]["initial_pose_2"]["qy"], config_data["visual_odometry"]["initial_pose_2"]["qz"]
+      }, Eigen::Vector3d(
+          config_data["visual_odometry"]["initial_pose_2"]["x"],
+          config_data["visual_odometry"]["initial_pose_2"]["y"],
+          config_data["visual_odometry"]["initial_pose_2"]["z"]
+      )
+  };
+  const VisualOdometryConfig visual_odometry_config{{{
+                                                         config_data["visual_odometry"]["initialize"]["keypoints"]["max_num"],
+                                                         config_data["visual_odometry"]["initialize"]["keypoints"]["quality_level"],
+                                                         config_data["visual_odometry"]["initialize"]["keypoints"]["min_distance_between"],
+                                                         config_data["visual_odometry"]["initialize"]["keypoints"]["edge_margin"],
+                                                         config_data["visual_odometry"]["initialize"]["keypoints"]["descriptor_angle_patch_radius"]
+                                                     }, {
+                                                         config_data["visual_odometry"]["initialize"]["matching"]["max_distance"],
+                                                         config_data["visual_odometry"]["initialize"]["matching"]["max_second_to_first_distance_ratio"],
+                                                         {
+                                                             config_data["visual_odometry"]["initialize"]["matching"]["ransac"]["threshold"],
+                                                             config_data["visual_odometry"]["initialize"]["matching"]["ransac"]["min_num_inliers"],
+                                                             config_data["visual_odometry"]["initialize"]["matching"]["ransac"]["max_num_iterations"],
+                                                         }},
+                                                     config_data["visual_odometry"]["initialize"]["max_num_bundle_adjustment_iterations"]
+
+                                                    }, {{
+                                                            config_data["visual_odometry"]["next"]["keypoints"]["max_num"],
+                                                            config_data["visual_odometry"]["next"]["keypoints"]["quality_level"],
+                                                            config_data["visual_odometry"]["next"]["keypoints"]["min_distance_between"],
+                                                            config_data["visual_odometry"]["next"]["keypoints"]["edge_margin"],
+                                                            config_data["visual_odometry"]["next"]["keypoints"]["descriptor_angle_patch_radius"]
+                                                        }, {
+                                                            config_data["visual_odometry"]["next"]["matching"]["max_image_distance"],
+                                                            config_data["visual_odometry"]["next"]["matching"]["max_descriptor_distance"],
+                                                            config_data["visual_odometry"]["next"]["matching"]["max_second_to_first_distance_ratio"],
+                                                            {
+                                                                config_data["visual_odometry"]["next"]["matching"]["ransac"]["max_num_iterations"],
+                                                                config_data["visual_odometry"]["next"]["matching"]["ransac"]["image_distance_threshold"],
+                                                            }}, {{
+                                                                     config_data["visual_odometry"]["next"]["keyframe"]["is_next"]["min_distance_to_last"],
+                                                                     config_data["visual_odometry"]["next"]["keyframe"]["is_next"]["max_num_landmark_keypoint_inliers"],
+                                                                     config_data["visual_odometry"]["next"]["keyframe"]["is_next"]["min_num_landmark_keypoint_inliers"],
+                                                                 },
+                                                                 config_data["visual_odometry"]["next"]["keyframe"]["max_num_keyframes"],
+                                                                 {
+                                                                     config_data["visual_odometry"]["next"]["keyframe"]["matching"]["max_distance"],
+                                                                     config_data["visual_odometry"]["next"]["keyframe"]["matching"]["max_second_to_first_distance_ratio"],
+                                                                     {
+                                                                         config_data["visual_odometry"]["next"]["keyframe"]["matching"]["ransac"]["threshold"],
+                                                                         config_data["visual_odometry"]["next"]["keyframe"]["matching"]["ransac"]["min_num_inliers"],
+                                                                         config_data["visual_odometry"]["next"]["keyframe"]["matching"]["ransac"]["max_num_iterations"],
+                                                                     }},
+                                                                 config_data["visual_odometry"]["next"]["keyframe"]["max_num_bundle_adjustment_iterations"],
+                                                        }}};
+
 
   // Alignment
-  const bool do_alignment{false};
-  const AlignConfig align_config;
+  const bool do_alignment{config_data["alignment"]["do_alignment"]};
+  const AlignConfig align_config{
+      static_cast<double>(config_data["alignment"]["align"]["distance_threshold_start"]) * voxel_size,
+      static_cast<double>(config_data["alignment"]["align"]["distance_threshold_end"]) * static_cast<double>(config_data["alignment"]["align"]["distance_threshold_start"]) * voxel_size,
+      config_data["alignment"]["align"]["max_num_alignment_steps"],
+      config_data["alignment"]["align"]["min_num_vertices_in_voxel"],
+      config_data["alignment"]["align"]["standard_deviation_scale"],
+      config_data["alignment"]["align"]["max_num_optimization_iterations"],
+  };
 
 
   ////////////////////////////////////////////////////////////////
   // Execution
 
   // General
-  fs::remove_all(output_path);
-  fs::create_directories(output_path / "visual_odometry_maps/pre_alignment");
-  if (do_alignment) fs::create_directories(output_path / "visual_odometry_maps/post_alignment");
+
+  if (do_dump_map) fs::create_directories(output_path / "visual_odometry_maps");
 
   // Generate the geometric map from the pointcloud.
   GeometricMap geometric_map{voxel_size, vertices_bounds};
@@ -110,8 +204,16 @@ int main() {
 
 
   // Run the visual odometry and align with the geometric map everytime a new keyframe was added.
-  std::vector<Pose> trajectory{visual_odometry.map.keyframes[initial_keyframe_index_1].pose, visual_odometry.map.keyframes[initial_keyframe_index_2].pose};
-  trajectory.reserve(image_paths.size());
+  std::vector<Pose> raw_trajectory{
+      visual_odometry.map.keyframes[initial_keyframe_index_1].pose,
+      visual_odometry.map.keyframes[initial_keyframe_index_2].pose
+  };
+  raw_trajectory.reserve(image_paths.size());
+  std::vector<Pose> transformed_trajectory{
+      visual_odometry.map.keyframes[initial_keyframe_index_1].pose,
+      visual_odometry.map.keyframes[initial_keyframe_index_2].pose
+  };
+  transformed_trajectory.reserve(image_paths.size());
 
   Sophus::Sim3d local_to_map_transformation{Eigen::Matrix4d::Identity()};
   std::vector<Sophus::Sim3d> transformations{local_to_map_transformation};
@@ -125,16 +227,16 @@ int main() {
     if (visual_odometry.last_keyframe_index != last_keyframe_index) {
       last_keyframe_index = visual_odometry.last_keyframe_index;
 
-      dump_map(
-          visual_odometry.map, output_path / fmt::format("visual_odometry_maps/pre_alignment/{}", last_keyframe_index));
+      if (do_dump_map) {
+        dump_map(
+            visual_odometry.map,
+            output_path / fmt::format("visual_odometry_maps/{}", last_keyframe_index));
+      }
 
       if (do_alignment) {
         // TODO: Check why optimization costs.
         align(local_to_map_transformation, geometric_map, visual_odometry.map, align_config);
 
-        dump_map(
-            visual_odometry.map,
-            output_path / fmt::format("visual_odometry_maps/post_alignment/{}", last_keyframe_index));
         transformations.push_back(local_to_map_transformation);
       }
 
@@ -164,13 +266,24 @@ int main() {
       if (!success) break;
     }
 
-    Pose transformed_pose = visual_odometry.current_pose;
-    transformed_pose.translation() = local_to_map_transformation * transformed_pose.translation();
-    trajectory.push_back(transformed_pose);
+    raw_trajectory.push_back(visual_odometry.current_pose);
+    if (do_alignment) {
+      Pose transformed_pose = visual_odometry.current_pose;
+      transformed_pose.translation() = local_to_map_transformation * transformed_pose.translation();
+      transformed_trajectory.push_back(transformed_pose);
+    }
   }
 
-  dump_trajectory(trajectory, output_path / "trajectory.csv");
+  dump_trajectory(
+      raw_trajectory, output_path / "raw_trajectory.csv", {initial_keyframe_index_1, initial_keyframe_index_2}
+  );
+
   if (do_alignment) {
+    dump_trajectory(
+        transformed_trajectory,
+        output_path / "transformed_trajectory.csv",
+        {initial_keyframe_index_1, initial_keyframe_index_2}
+    );
     dump_transformations(transformations, output_path / "transformations.csv");
   }
 
